ec3ad703f0e3971735d5be9a98c4481d40c646db
Rajpaveen2005 <rajpaveen2005@gmail.com>
2024-07-23 09:52:13 +0530

    setup

--- camps/src/app/login/login.component.spec.ts
+++ camps/src/app/login/login.component.spec.ts
@@ -9,6 +9,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent]
+      imports: [loginComponent]
     })
     .compileComponents();
 
